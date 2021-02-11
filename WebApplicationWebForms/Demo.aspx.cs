9b7bd98a61d00c0f18bf5c64b6b2aea3054144d4
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 15:14:54 +0100

    Connexion base de donnees SQL Server + SSMS

--- WebApplicationWebForms/Demo.aspx.cs
+++ WebApplicationWebForms/Demo.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace WebApplicationWebForms
 {
@@ -12,6 +13,17 @@ namespace WebApplicationWebForms
         public object date;
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Server=localhost\SQLEXPRESS01;Trusted_Connection=True;Database=TestCSharp ;User ID=testC;Password=csharp";
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+            Response.Write("Connexion réussie");
+            cnn.Close();
+
             this.date = DateTime.Now;
 
             Session["Name"] = txtName.Text;
